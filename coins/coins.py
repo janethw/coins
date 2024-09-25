4717d29e70d18f39360192e81c77abef0d0bdb5b
Janet Hamilton-Wilks <janethw@users.noreply.github.com>
2024-09-25 13:34:07 +0100

    Refinement of code and test cases to test that value is an integer and >= 0.

--- coins/coins.py
+++ coins/coins.py
@@ -1,7 +1,28 @@
+def main():
+    value = -2
+    try:
+        check_value_is_int(value)
+        check_value_is_positive_or_zero(value)
+    except ValueError as value_error:
+        print(f"Value is not valid; {value_error}")
 
-def coin_calculator(input_value):
-    if input_value >= 0:
-        target_value = input_value
-    else:
-        target_value = -1
-    return target_value
+
+# Check target value is an integer
+def check_value_is_int(input_value):
+    if not isinstance(input_value, int):
+        print(-1)
+        raise ValueError("Value, V, must be expressed in terms of unit currency, "
+                         "eg whole pence for Pound Sterling")
+    return input_value
+
+
+# Check target value is positive or zero
+def check_value_is_positive_or_zero(input_value):
+    if input_value < 0:
+        print(-1)
+        raise ValueError("Value, V, must be positive or zero")
+    return input_value
+
+
+if __name__ == "__main__":
+    main()
--- coins/test_coins.py
+++ coins/test_coins.py
@@ -1,20 +1,29 @@
 import unittest
 
-from coins import coin_calculator
+from coins import check_value_is_int, check_value_is_positive_or_zero
 
 
 class TestTargetValue(unittest.TestCase):
-    def test_input_value_is_positive_or_zero(self):
-        self.assertEqual(coin_calculator(5), 5)
-        self.assertEqual(coin_calculator(-4), -1, "Value must be a positive amount")
-        self.assertEqual(coin_calculator(0), 0)
 
     def test_input_value_is_an_int(self):
-        self.assertIsInstance(coin_calculator(4), int)
-        self.assertIsInstance(coin_calculator(-2), int, "An int, but must be positive")
-        self.assertIsInstance(coin_calculator(4.23 * 100), int)  # AssertionError: 423.00000000000006 is not an instance of <class 'int'>
-        self.assertIsInstance(coin_calculator(4.23 * 100 % 100), int)  # AssertionError: 23.000000000000057 is not an instance of <class 'int'>
-        self.assertIsInstance(coin_calculator(4.0), int)  # AssertionError: 4.0 is not an instance of <class 'int'>
+        # Positive test case
+        self.assertIsInstance(check_value_is_int(4), int)
+        self.assertIsInstance(check_value_is_int(-2), int)
+
+        # Negative test case
+        # self.assertIsInstance(check_value_is_int(4.23 * 100), int)  # AssertionError: 423.00000000000006 is not an instance of <class 'int'>
+        # self.assertIsInstance(check_value_is_int(4.23 * 100 % 100), int)  # AssertionError: 23.000000000000057 is not an instance of <class 'int'>
+        # self.assertIsInstance(check_value_is_int(4.0), int)  # AssertionError: 4.0 is not an instance of <class 'int'>
+
+    def test_input_value_is_positive_or_zero(self):
+        # Positive test case
+        self.assertEqual(check_value_is_positive_or_zero(5), 5)
+        self.assertEqual(check_value_is_positive_or_zero(0), 0)
+
+        # # Negative test case
+        with self.assertRaises(ValueError) as context:
+            check_value_is_positive_or_zero(-4)
+        self.assertEqual(str(context.exception), "Value, V, must be positive or zero")
 
 
 if __name__ == "__main__":
