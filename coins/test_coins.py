74f4c1922894f473bdd937896ff22bd43d12649e
Janet Hamilton-Wilks <janethw@users.noreply.github.com>
2024-09-26 11:23:40 +0100

    Completed positive and negative unittests for 3 check_value functions

--- coins/test_coins.py
+++ coins/test_coins.py
@@ -1,19 +1,42 @@
 import unittest
 
-from coins import check_value_is_int, check_value_is_positive_or_zero
+from coins import check_value_is_int, check_value_is_positive_or_zero, check_value_is_in_range
 
 
 class TestTargetValue(unittest.TestCase):
+
+    def test_check_value_is_in_range(self):
+        # Positive test case
+        self.assertLessEqual(check_value_is_in_range(10 ** 15), 10 ** 15)
+        self.assertLessEqual(check_value_is_int(12 ** 4), 12 ** 4)
+        self.assertLessEqual(check_value_is_int(12), 12)
+
+        # Negative test case
+        negative_test_cases_for_range = [-1, 10 ** 16, -10 ** 16, -4 ** 11]
+        for test_value in negative_test_cases_for_range:
+            print(f"Current test: {test_value=}")
+            with self.assertRaises(ValueError) as context:
+                check_value_is_in_range(test_value)
+            self.assertEqual(str(context.exception),
+                             "Value, V, must be between 0 and 10 to the power 15")
 
     def test_input_value_is_an_int(self):
         # Positive test case
         self.assertIsInstance(check_value_is_int(4), int)
         self.assertIsInstance(check_value_is_int(-2), int)
+        self.assertIsInstance(check_value_is_int(10 ** 15), int)
+        self.assertIsInstance(check_value_is_int(10 ** 16), int)
 
         # Negative test case
         # self.assertIsInstance(check_value_is_int(4.23 * 100), int)  # AssertionError: 423.00000000000006 is not an instance of <class 'int'>
         # self.assertIsInstance(check_value_is_int(4.23 * 100 % 100), int)  # AssertionError: 23.000000000000057 is not an instance of <class 'int'>
-        # self.assertIsInstance(check_value_is_int(4.0), int)  # AssertionError: 4.0 is not an instance of <class 'int'>
+        negative_test_cases_for_int = [4.23, 4.23**3, 4.23 * 100, 4.23 * 100 % 100]
+        for test_value in negative_test_cases_for_int:
+            print(f"Current test: {test_value=}")
+            with self.assertRaises(ValueError) as context:
+                check_value_is_int(test_value)
+            self.assertEqual(str(context.exception),
+                             "Value, V, must be expressed in terms of unit currency, eg whole pence for Pound Sterling")
 
     def test_input_value_is_positive_or_zero(self):
         # Positive test case
