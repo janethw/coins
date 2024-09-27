81f8f7972574498ff4ac0986190170bfabfa1232
Janet Hamilton-Wilks <janethw@users.noreply.github.com>
2024-09-27 17:41:34 +0100

    Refactoring coins.py and unittests to reuse, where possible, the validity functions for valueV to check the validity of elements in the coins array.

--- coins/coins.py
+++ coins/coins.py
@@ -2,13 +2,13 @@ from denominations import Denomination
 
 
 def main():
-    value = 10 ** 2
+    valueV = 10 ** 2
     coins = [1, 2, 5, 10, 20, 50, 100, 200]
 
     # Validate value (V)
     try:
-        print(f"{value=}")
-        check_value_is_valid(value)
+        print(f"{valueV=}")
+        check_value_is_valid(valueV, variable_name="V")
     except ValueError as value_error:
         print(-1)
         print(f"Value is not valid: {value_error}")
@@ -29,49 +29,49 @@ def main():
 
     # Validate coins array
     try:
-        check_coins_array_is_valid(coins)
+        check_coins_array_is_valid(coins, variable_name="coins array")
     except ValueError as value_error:
         print(-1)
         print(f"The coins array is not valid: {value_error}")
 
 
-def check_value_is_valid(input_value):
-    check_value_is_in_range(input_value)
-    check_value_is_int(input_value)
-    check_value_is_positive_or_zero(input_value)
+def check_value_is_valid(input_value, variable_name):
+    check_value_is_in_range(input_value, variable_name)
+    check_value_is_int(input_value, variable_name)
+    check_value_is_positive(input_value, variable_name)
     return input_value
 
 
 # Check target value is in range
-def check_value_is_in_range(input_value):
+def check_value_is_in_range(input_value, variable_name):
     if not (0 <= input_value <= 10 ** 15):
         print(-1)
-        raise ValueError("Value, V, must be between 0 and 10 to the power 15")  # Equivalent to GBP 10 Trillion
+        raise ValueError(f"Currency amounts for {variable_name} must be between 0 and 10 to the power 15")  # Equivalent to GBP 10 Trillion
     return input_value
 
 
 # Check target value is an integer
-def check_value_is_int(input_value):
+def check_value_is_int(input_value, variable_name):
     if not isinstance(input_value, int):
         print(-1)
-        raise ValueError("Value, V, must be expressed in terms of unit currency, "
-                         "eg whole pence for Pound Sterling")
+        raise ValueError(f"Currency amounts for {variable_name} must be expressed in terms of unit currency, "
+                         "eg multiples of whole pence for Pound Sterling")
     return input_value
 
 
 # Check target value is positive or zero
-def check_value_is_positive_or_zero(input_value):
-    if input_value < 0:
+def check_value_is_positive(input_value, variable_name):
+    if input_value <= 0:
         print(-1)
-        raise ValueError("Value, V, must be positive or zero")
+        raise ValueError(f"Currency amounts for {variable_name} must be positive")
     return input_value
 
 
-def check_coins_array_is_valid(coins):
+def check_coins_array_is_valid(coins, variable_name):
     for coin in coins:
-        check_positive_integers(coin)
-        check_array_length(coin)
-        check_large_denominations(coin)
+        check_value_is_int(coin, variable_name)
+        check_array_length(coin, variable_name)
+        check_value_is_in_range(coin, variable_name)
     return coins
 
 
@@ -92,18 +92,8 @@ def remove_duplicates(coin_array):
     return unique_coin_array
 
 
-# Validation of coins[] - check denominations are positive integers
-def check_positive_integers(coin):
-    pass
-
-
 # Validation of coins[] - check of array length
-def check_array_length(coin):
-    pass
-
-
-# Validation of coins[] - check of denomination amounts
-def check_large_denominations(coin):
+def check_array_length(coins, variable_name):
     pass
 
 
--- coins/test_coins.py
+++ coins/test_coins.py
@@ -1,53 +1,57 @@
 import unittest
 
-from coins import (check_value_is_int, check_value_is_positive_or_zero, check_value_is_in_range, remove_duplicates,
+from coins import (check_value_is_int, check_value_is_positive, check_value_is_in_range, remove_duplicates,
                    check_coins_is_list)
 
 
 class TestTargetValue(unittest.TestCase):
 
-    def test_check_value_is_in_range(self):
-        # Positive test case
-        self.assertLessEqual(check_value_is_in_range(10 ** 15), 10 ** 15)
-        self.assertLessEqual(check_value_is_int(12 ** 4), 12 ** 4)
-        self.assertLessEqual(check_value_is_int(12), 12)
+    def test_check_value_is_in_range(self, variable_name="V"):
+        # Positive test case for valueV
+        self.assertLessEqual(check_value_is_in_range(10 ** 15, variable_name), 10 ** 15)
+        self.assertLessEqual(check_value_is_in_range(12 ** 4, variable_name), 12 ** 4)
+        self.assertLessEqual(check_value_is_in_range(12, variable_name), 12)
 
         # Negative test case
         negative_test_cases_for_range = [-1, 10 ** 16, -10 ** 16, -4 ** 11]
         for test_value in negative_test_cases_for_range:
             print(f"Current test: {test_value=}")
             with self.assertRaises(ValueError) as context:
-                check_value_is_in_range(test_value)
+                check_value_is_in_range(test_value, variable_name)
             self.assertEqual(str(context.exception),
-                             "Value, V, must be between 0 and 10 to the power 15")
+                             f"Currency amounts for {variable_name} must be between 0 and 10 to the power 15")
 
-    def test_input_value_is_an_int(self):
+    def test_input_value_is_an_int(self, variable_name="V"):
         # Positive test case
-        self.assertIsInstance(check_value_is_int(4), int)
-        self.assertIsInstance(check_value_is_int(-2), int)
-        self.assertIsInstance(check_value_is_int(10 ** 15), int)
-        self.assertIsInstance(check_value_is_int(10 ** 16), int)
+        self.assertIsInstance(check_value_is_int(4, variable_name), int)
+        self.assertIsInstance(check_value_is_int(-2, variable_name), int)
+        self.assertIsInstance(check_value_is_int(10 ** 15, variable_name), int)
+        self.assertIsInstance(check_value_is_int(10 ** 16, variable_name), int)
 
         # Negative test case
-        # self.assertIsInstance(check_value_is_int(4.23 * 100), int)  # AssertionError: 423.00000000000006 is not an instance of <class 'int'>
-        # self.assertIsInstance(check_value_is_int(4.23 * 100 % 100), int)  # AssertionError: 23.000000000000057 is not an instance of <class 'int'>
         negative_test_cases_for_int = [4.23, 4.23**3, 4.23 * 100, 4.23 * 100 % 100]
         for test_value in negative_test_cases_for_int:
             print(f"Current test: {test_value=}")
             with self.assertRaises(ValueError) as context:
-                check_value_is_int(test_value)
+                check_value_is_int(test_value, variable_name)
             self.assertEqual(str(context.exception),
-                             "Value, V, must be expressed in terms of unit currency, eg whole pence for Pound Sterling")
+                             f"Currency amounts for {variable_name} must be expressed in terms of unit currency, "
+                             f"eg multiples of whole pence for Pound Sterling")
+        # Examples of early test fails
+        # self.assertIsInstance(check_value_is_int(4.23 * 100), int)  # AssertionError:
+        # 423.00000000000006 is not an instance of <class 'int'>
+        # self.assertIsInstance(check_value_is_int(4.23 * 100 % 100), int)  # AssertionError:
+        # 23.000000000000057 is not an instance of <class 'int'>
 
-    def test_input_value_is_positive_or_zero(self):
-        # Positive test case
-        self.assertEqual(check_value_is_positive_or_zero(5), 5)
-        self.assertEqual(check_value_is_positive_or_zero(0), 0)
+    def test_input_value_is_positive(self, variable_name="V"):
+        # Positive test case for valueV
+        self.assertEqual(check_value_is_positive(5, variable_name), 5)
+        self.assertEqual(check_value_is_positive(1, variable_name), 1)
 
         # # Negative test case
         with self.assertRaises(ValueError) as context:
-            check_value_is_positive_or_zero(-4)
-        self.assertEqual(str(context.exception), "Value, V, must be positive or zero")
+            check_value_is_positive(-4, variable_name)
+        self.assertEqual(str(context.exception), f"Currency amounts for {variable_name} must be positive")
 
 
 class TestDenominationClass(unittest.TestCase):
@@ -68,6 +72,13 @@ class TestDenominationClass(unittest.TestCase):
         self.assertEqual(remove_duplicates([0, 1, 2, 2, 2, 5, 10, 100]), [0, 1, 2, 5, 10, 100])
         self.assertEqual(remove_duplicates([1, 1, 1, 2, 5, 10, 50, 100, 200, 200, 200, 2 * 10 ** 2]),
                          [1, 2, 5, 10, 50, 100, 200])
+
+    def test_input_value_is_positive(self, variable_name="coins array"):
+        # Positive test case for coins array
+        self.assertEqual(check_value_is_positive(5, variable_name), 5)
+        self.assertEqual(check_value_is_positive(1, variable_name), 1)
+
+        # Negative test case
 
 
 if __name__ == "__main__":
